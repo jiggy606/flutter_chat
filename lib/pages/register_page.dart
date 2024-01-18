ffc5bbaf527739a94230362027a3e46e792942a4
Iberi Daniel <danieliberi99@gmail.com>
2024-01-18 11:22:18 +0100

    finished register page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -2,19 +2,20 @@ import 'package:chat_app/components/button.dart';
 import 'package:chat_app/components/text_field.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
+class RegisterPage extends StatefulWidget {
   final Function()? onTap;
-  const LoginPage({super.key, required this.onTap});
+  const RegisterPage({super.key, required this.onTap});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
   // text editing controllers
   final emailTextController = TextEditingController();
   final passwordTextController = TextEditingController();
-  
+  final confirmPasswordTextFieldController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,60 +32,73 @@ class _LoginPageState extends State<LoginPage> {
                   Icons.lock,
                   size: 100,
                 ),
-          
+
                 const SizedBox(
                   height: 50,
                 ),
-          
+
                 // welcome
-                Text("Welcome back, you have been missed!", style: TextStyle(color: Colors.grey[700]),),
-          
+                Text(
+                  "Let's create an account for you  ",
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+
                 const SizedBox(
                   height: 25,
                 ),
-          
+
                 // email
                 MyTextField(
                     controller: emailTextController,
                     hintText: 'Email',
-                    obsecureText: false
-                ),
+                    obsecureText: false),
 
                 const SizedBox(
                   height: 10,
                 ),
-          
+
                 // password
                 MyTextField(
                     controller: passwordTextController,
                     hintText: 'Password',
-                    obsecureText: true
+                    obsecureText: true),
+
+                const SizedBox(
+                  height: 10,
                 ),
+
+                // confirm password
+                MyTextField(
+                    controller: confirmPasswordTextFieldController,
+                    hintText: 'Confirm Password',
+                    obsecureText: true),
 
                 const SizedBox(
                   height: 25,
                 ),
-          
+
                 // sign in
-                MyButton(
-                  onTap: (){}, 
-                  text: 'Sign In'
-                ),
+                MyButton(onTap: () {}, text: 'Sign Up'),
 
                 const SizedBox(
                   height: 25,
                 ),
-          
+
                 // register
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("Not a member?", style: TextStyle(color: Colors.grey[700]),),
-                    const SizedBox(width: 4,),
+                    Text(
+                      "Already have an account?",
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                    const SizedBox(
+                      width: 4,
+                    ),
                     GestureDetector(
                       onTap: widget.onTap,
                       child: const Text(
-                        "Register Now",
+                        "Login",
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           color: Colors.blue,
