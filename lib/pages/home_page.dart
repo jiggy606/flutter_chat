e6a638f25c542bf6a8f6e175759ea30ed9d185fd
Iberi Daniel <beltheshazzarr@MacBook-Air.local>
2024-02-23 10:21:32 +0100

    added postability

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,8 @@
+import 'dart:developer';
+
+import 'package:chat_app/components/text_field.dart';
+import 'package:chat_app/components/wall_post.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +14,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // current user
+  final currentUser = FirebaseAuth.instance.currentUser!;
+
+  // text controller
+  final textController = TextEditingController();
+
+  // message control
+  void postMessage() {
+    // only posts is textfield contains data
+    if (textController.text.isNotEmpty) {
+      // store in firebase
+      FirebaseFirestore.instance.collection("User Posts").add({
+        "UserEmail": currentUser.email,
+        "Message": textController.text,
+        "TimeStamp": Timestamp.now()
+      });
+    }
+  }
+
   // sign user out
   void signOut() {
     FirebaseAuth.instance.signOut();
@@ -17,10 +41,75 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
         title: Text("The wall"),
+        backgroundColor: Colors.grey[800],
         // sign out button
         actions: [IconButton(onPressed: signOut, icon: Icon(Icons.logout))],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            // the wall
+            Expanded(
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection("User Posts")
+                    .orderBy(
+                      "TimeStamp",
+                      descending: false,
+                    )
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                      // get message
+                      final post = snapshot.data!.docs[index];
+                      return WallPost(
+                        message: post["Message"],
+                        user: post["UserEmail"],
+                      );
+                    });
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text('Error:${snapshot.error}'),
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+
+            // message post
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                children: [
+                  //expand textfield
+                  Expanded(
+                    child: MyTextField(
+                      controller: textController,
+                      hintText: "Wite something on here... ",
+                      obsecureText: false,
+                    ),
+                  ),
+
+                  IconButton(
+                      onPressed: postMessage,
+                      icon: const Icon(Icons.arrow_circle_up))
+                ],
+              ),
+            ),
+
+            // logged in as "user name"
+            Text("Logged in as: " + currentUser.email!)
+          ],
+        ),
       ),
     );
   }
