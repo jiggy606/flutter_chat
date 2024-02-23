e4d2b6c326ca947ddf2a4d69effb14c0dedb1225
Iberi Daniel <beltheshazzarr@MacBook-Air.local>
2024-02-23 08:55:24 +0100

    cpmpleted user login and registeration

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/components/button.dart';
 import 'package:chat_app/components/text_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -15,6 +16,52 @@ class _RegisterPageState extends State<RegisterPage> {
   final emailTextController = TextEditingController();
   final passwordTextController = TextEditingController();
   final confirmPasswordTextFieldController = TextEditingController();
+
+  // user sign up setup
+  void signUp() async {
+    // show loading circle
+    showDialog(
+        context: context,
+        builder: (context) => const Center(
+              child: CircularProgressIndicator(),
+            ));
+
+    // make sure passwords match
+    if (passwordTextController.text !=
+        confirmPasswordTextFieldController.text) {
+      // remove loading circle
+      Navigator.pop(context);
+      // shoe error
+      messageDisplay("Passwords do not match");
+      return;
+    }
+
+    // creating the user
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailTextController.text,
+        password: passwordTextController.text
+      );
+
+      // remove loading circle
+      if (context.mounted) Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      // remove loading circle
+      Navigator.pop(context);
+      // show error message
+      messageDisplay(e.code);
+    }
+  }
+
+  // display message
+  void messageDisplay(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(message),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,8 +124,8 @@ class _RegisterPageState extends State<RegisterPage> {
                   height: 25,
                 ),
 
-                // sign in
-                MyButton(onTap: () {}, text: 'Sign Up'),
+                // sign up button
+                MyButton(onTap: signUp, text: 'Sign Up'),
 
                 const SizedBox(
                   height: 25,
